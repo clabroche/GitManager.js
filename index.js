b52f8518a75e70c316cfea8eb9b214a427104879
labroche <corentin.labroche@inist.fr>
2017-10-30 02:35:07 +0100

    add documentation

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const path = require("path");
 const Promise = require("bluebird");
 const fs = Promise.promisifyAll(require("fs"));
 
-// tested
-const GitManager = function(gitLocation = path.resolve("./")) {
+/**
+ * @constructor 
+ * @param {String} gitLocation path to a repository
+ */
+function GitManager(gitLocation = path.resolve("./")) {
   this.gitLocation = path.resolve(gitLocation);
   if (!fs.existsSync(path.resolve(this.gitLocation, ".git")))
     throw new Error("can't find .git repository");
-  this.local = new (require("./_local"))(this.gitLocation);
-  this.remote = new (require("./_remote"))({
+  this.local = new (require("./lib/local"))(this.gitLocation);
+  this.remote = new (require("./lib/remote"))({
     gitLocation: this.gitLocation,
     localFunction: this.local
   });
